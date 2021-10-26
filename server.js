82a5a0044c6ff8ef9f70f5cb1f8eb73443527dad
svg23 <subhavgopal23@gmail.com>
2021-10-26 23:27:51 +0530

    ejs

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.use(express.json());
 app.listen(3000,()=>{
     console.log("started");
 })
+app.set('view engine', 'ejs');
 const mysql=require("mysql2");
 var con = mysql.createConnection({
     host: "localhost",
@@ -20,10 +21,29 @@ con.query("use iolx", function (err, result) {
     if (err) throw err;
     console.log("Database connected");
   });
+var products=[];
+var name="";
+var id="";
 app.use(express.static("public"))
 app.get("/",function(req,res){
     // console.log(__dirname)
-    res.sendFile(__dirname+"/frontend/2.html");
+    res.render("home",{name:name,id:id});
+})
+app.get("/sell",function(req,res){
+    res.render("sell",{name:name,id:id});
+})
+app.get("/form",function(req,res){
+    res.render("sellForm",{name:name,id:id,products:products});
+})
+app.post("/form",function(req,res){
+    console.log(req.body);
+    var name=req.body.title;
+    var desc=req.body.desc;
+    var price=req.body.price;
+    var pic=req.body.pic;
+    values={name,desc,price,id};
+    products.push(values);
+    res.render("sellForm",{name:name,id:id,products:products});
 })
 app.get("/register",function(req,res){
     res.sendFile(__dirname+"/frontend/Registration.html");
@@ -48,11 +68,13 @@ app.post("/register",function(req,res){
                 con.query(q,[value],function (err) {
                 if (err) {throw err;}
                 console.log("success");
+                res.redirect("/");
                 })
             }
             else{
                 console.log(result);
                 console.log("email already in use");
+                res.redirect("/register");
             }
         })
     }
@@ -67,11 +89,14 @@ app.post("/login",function(req,res){
     con.query("SELECT * from user where email=? AND password=?",[email,password],function(err,result){
         if(err) throw err;
         if(result.length==1){
-            console.log(result);
             console.log("success");
+            name=result[0].name;
+            id=result[0].id;
+            res.render("home",{name:name,id:id});
         }
         else{
             console.log("invalid");
+            res.redirect("/login")
         }
     })
 })
