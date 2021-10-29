ae99bd75975a445adb32fd1771cf509ed3a772b5
svg23 <subhavgopal23@gmail.com>
2021-10-29 14:08:26 +0530

    productListInSQL

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.listen(3000,()=>{
     console.log("started");
 })
 app.set('view engine', 'ejs');
+var helper=require("./helper")
 const mysql=require("mysql2");
 var con = mysql.createConnection({
     host: "localhost",
@@ -23,7 +24,20 @@ con.query("use iolx", function (err, result) {
   });
 var products=[];
 var name="";
-var id="";
+var id=0;
+function addProduct(product){
+    con.query("INSERT INTO listing (productName,price,details,sellerID) VALUES ?",[product],function(err,result){
+        if (err) throw err;
+        console.log(result);
+    })
+}
+function deleteProduct(productName,price,id){
+    console.log("b");
+    // con.query("DELETE from listing WHERE productName=? AND sellerID=? AND price=? ",[productName,id,price],function(err,result){
+    //     if(err) throw err;
+    //     console.log(result);
+    // })
+}
 app.use(express.static("public"))
 app.get("/",function(req,res){
     // console.log(__dirname)
@@ -33,18 +47,25 @@ app.get("/sell",function(req,res){
     res.render("sell",{name:name,id:id});
 })
 app.get("/form",function(req,res){
-    res.render("sellForm",{name:name,id:id,products:products});
-})
+    res.render("sellForm",{name:name,id:id,products:products});})
 app.post("/form",function(req,res){
-    console.log(req.body);
     var name=req.body.title;
     var desc=req.body.desc;
     var price=req.body.price;
     var pic=req.body.pic;
-    values={name,desc,price,id};
-    products.push(values);
-    res.render("sellForm",{name:name,id:id,products:products});
-})
+    // deleteProduct(name,price,id);
+    if(id==0){
+        console.log("not logged in");
+        res.redirect("/login")
+    }
+    else{
+        product=[[name,price,"whdgs",id]];
+        addProduct(product);
+        values={"name":name,"price":price,"userId":id};
+        products.push(values);
+        res.render("sellForm",{name:name,id:id,products:products});
+    }
+    })
 app.get("/register",function(req,res){
     res.sendFile(__dirname+"/frontend/Registration.html");
 })
@@ -92,7 +113,7 @@ app.post("/login",function(req,res){
             console.log("success");
             name=result[0].name;
             id=result[0].id;
-            res.render("home",{name:name,id:id});
+            res.render("home",{name:name,id:id,products:products});
         }
         else{
             console.log("invalid");
